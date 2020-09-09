bc5d9f5e7900c6c5bd2435d0f9efb4ef9e48154b
syedsabir06 <syed.sabir06@gmail.com>
2020-09-10 00:00:32 +0530

    First Commit

--- app/src/main/java/in/prali/one/OneToast.java
+++ app/src/main/java/in/prali/one/OneToast.java
@@ -3,7 +3,10 @@ import android.content.Context;
 import android.widget.Toast;
 
 public class OneToast {
+
+
     public OneToast (Context context, String message){
+
         Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
     }
 }
