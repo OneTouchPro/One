5961f18f4c73b67291cd5242a6527a367578517b
PRALI <syed.sabir06@gmail.com>
2021-08-30 19:39:49 +0530

    Context Updated

--- app/src/main/java/in/prali/one/OneToast.java
+++ app/src/main/java/in/prali/one/OneToast.java
@@ -1,13 +1,14 @@
 package in.prali.one;
 
+import android.app.Activity;
 import android.content.Context;
 import android.widget.Toast;
 
+import static java.security.AccessController.getContext;
+
 public class OneToast {
 
-    private Context context;
-
-    public OneToast(String message) {
+    public OneToast(Context context, String message) {
         Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
     }
 
